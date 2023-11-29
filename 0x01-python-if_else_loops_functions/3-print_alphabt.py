93c23981a8d24e704f04697d57920d7eab227a61
gitAkin <akinwumialalade@yahoo.com>
2023-11-28 20:37:31 -0800

    program that does not print q and e

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z') + 1):
-    if chr(i) in "qe":
-        continue
-    print("{}".format(chr(i)), end="")
+for ch in range(97, 123):
+    if chr(ch) != 'q' and chr(ch) != 'e':
+        print("{}".format(chr(ch)), end='')
