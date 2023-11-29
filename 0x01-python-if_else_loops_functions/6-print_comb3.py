a9a6a560173643e6d049d58175920584d4d3ecc4
gitAkin <akinwumialalade@yahoo.com>
2023-11-28 20:51:09 -0800

    program that prints different possible numbers

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 10):
     for j in range(i+1, 10):
-        if (i != 8) or (j != 9):
-            print("{:d}{:d}".format(i, j), end=", ")
+        if (i == 8) or (j == 9):
+		print("{}{}".format(num1, num2))
         else:
-            print("{:d}{:d}".format(i, j))
+           	print("{}{}".format(num1, num2), end=", ")
